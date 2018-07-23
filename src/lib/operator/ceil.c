318f840c7b2fa82898ba4c8312a2016da9df58d6
John <k1234233@gmail.com>
2018-07-23 18:32:21 +0800

    ceil finished

--- src/lib/operator/ceil.c
+++ src/lib/operator/ceil.c
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <operator/ceil.h>
-
+#include <math.h>
 #include <stdint.h>
 #include <stdbool.h>
 
@@ -13,4 +13,15 @@ void ONNC_RUNTIME_ceil_float(
   ,int32_t output_Y_ndim, const int32_t * restrict output_Y_dims
   
 ) {
+
+	int32_t size = 1;
+	for(int32_t i = 0; i < input_X_ndim; ++i){
+		size *= input_X_dims[i];  
+	}
+	
+	for(int32_t i = 0; i<size; ++i){
+
+		output_Y[i] = ceil(input_X[i]);
+	}
+
 }
