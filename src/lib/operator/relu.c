eab850f9485be2016b2941124531404aea64dd5e
Akio Lin <akioolin@gmail.com>
2018-07-24 23:46:27 +0800

    Add operator relu

--- src/lib/operator/relu.c
+++ src/lib/operator/relu.c
@@ -13,4 +13,17 @@ void ONNC_RUNTIME_relu_float(
   ,int32_t output_Y_ndim, const int32_t * restrict output_Y_dims
   
 ) {
+
+	int32_t size = 1;
+	int32_t numofdim = input_X_ndim;
+
+	for(int32_t i = 0 ; i < numofdim ; ++i){
+		size *= input_X_dims[i];
+	}
+
+	for(int32_t i = 0 ; i < size ; ++i){
+	    float tmp_val = input_X[i];
+		output_Y[i] = (tmp_val >= 0.0f) ? tmp_val : 0.0f;
+	}
 }
+
