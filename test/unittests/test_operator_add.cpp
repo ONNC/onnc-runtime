6ce0878b5ebef46a9c14562909ae0d324cce90c5
LuisHsu <luishsuplus@gmail.com>
2018-07-24 11:21:06 +0800

    Unittest of abs acos add asin

---
+++ test/unittests/test_operator_add.cpp
@@ -0,0 +1,47 @@
+#include <skypat/skypat.h>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+
+#define restrict __restrict__
+extern "C"{
+#include <operator/add.h>
+}
+#undef restrict
+
+SKYPAT_F(Operator_Add, non_broadcast){
+    // Prepare
+    srand(time(NULL));
+    int32_t ndim = rand() % 3 + 1;
+    int32_t dims[ndim];
+    int32_t dataSize = 1;
+    for(int32_t i = 0; i < ndim; ++i){
+        dims[i] = rand() % 100 + 1;
+        dataSize *= dims[i];
+    }
+    float A[dataSize], B[dataSize], C[dataSize], Ans[dataSize];
+    for(int32_t i = 0; i < dataSize; ++i){
+        A[i] = rand() % 1000 / 100;
+        B[i] = rand() % 1000 / 100;
+        Ans[i] = A[i] + B[i];
+    }
+    // Run
+    ONNC_RUNTIME_add_float(NULL
+        ,A
+        ,ndim,dims
+        ,B
+        ,ndim,dims
+        ,C
+        ,ndim,dims
+    );
+    // Check
+    for(int32_t i = 0; i < dataSize; ++i){
+        EXPECT_EQ(C[i], Ans[i]);
+    }
+}
+
+int main(int argc, char *argv[]){
+    skypat::Test::Initialize(argc, argv);
+    skypat::Test::RunAll();
+    return (skypat::testing::UnitTest::self()->getNumOfFails() == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
