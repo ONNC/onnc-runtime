a37ee09d3ec09cdd6e38b0872a85232b113c76aa
tall15421542 <b04703001@ntu.edu.tw>
2018-07-24 16:26:01 +0800

    initial exp.c

--- src/lib/operator/exp.c
+++ src/lib/operator/exp.c
@@ -11,6 +11,14 @@ void ONNC_RUNTIME_exp_float(
   ,int32_t input_input_ndim, const int32_t * restrict input_input_dims
   ,float * restrict output_output
   ,int32_t output_output_ndim, const int32_t * restrict output_output_dims
-  
+
 ) {
+    int size = 1;
+    for(int dim = 0 ; dim < input_input_ndim ; dim++){
+        size *= input_input_dims[dim];
+    }
+
+    for(int index = 0 ; index < size ; index++){
+        output_output[index] = exp(input_input[index]);
+    }
 }
