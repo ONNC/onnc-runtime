f9d725cecf23310c7d6d352ae97db7310559cbbf
Akio Lin <akioolin@gmail.com>
2018-07-25 23:40:16 +0800

    add operator reciprocal

--- src/lib/operator/reciprocal.c
+++ src/lib/operator/reciprocal.c
@@ -4,6 +4,7 @@
 
 #include <stdint.h>
 #include <stdbool.h>
+#include <math.h>
 
 void ONNC_RUNTIME_reciprocal_float(
   void * restrict onnc_runtime_context
@@ -13,4 +14,18 @@ void ONNC_RUNTIME_reciprocal_float(
   ,int32_t output_Y_ndim, const int32_t * restrict output_Y_dims
   
 ) {
+	int32_t size = 1;
+	int32_t numofdim = input_X_ndim;
+
+	for(int32_t i = 0 ; i < numofdim ; ++i){
+		size *= input_X_dims[i];
+	}
+
+    // y = 1/x, x!= 0.0f
+    // y = INF, if x = 0.0f    
+	for(int32_t i = 0 ; i < size ; ++i){
+		float tmp_val = input_X[i];
+		output_Y[i] = tmp_val == 0.0f ? INFINITY : 1.0f / tmp_val;
+	}
 }
+
--- src/lib/operator/thresholdedrelu.c
+++ src/lib/operator/thresholdedrelu.c
@@ -13,4 +13,17 @@ void ONNC_RUNTIME_thresholdedrelu_float(
   ,int32_t output_Y_ndim, const int32_t * restrict output_Y_dims
   ,float alpha
 ) {
+
+	int32_t size = 1;
+	int32_t numofdim = input_X_ndim;
+
+	for(int32_t i = 0 ; i < numofdim ; ++i){
+		size *= input_X_dims[i];
+	}
+
+	for(int32_t i = 0 ; i < size ; ++i){
+	    float tmp_val = input_X[i];
+		output_Y[i] = (tmp_val >= alpha) ? tmp_val : 0.0f;
+	}
 }
+
