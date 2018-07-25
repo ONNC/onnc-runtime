2069debbc6adea0aa9282a3325604eae220bbf26
tall15421542 <b04703001@ntu.edu.tw>
2018-07-25 11:33:28 +0800

    initial shape

--- src/lib/operator/shape.c
+++ src/lib/operator/shape.c
@@ -9,6 +9,9 @@ void ONNC_RUNTIME_shape_float(
   ,int32_t input_data_ndim, const int32_t * restrict input_data_dims
   ,float * restrict output_shape
   ,int32_t output_shape_ndim, const int32_t * restrict output_shape_dims
-  
+
 ) {
+    for(int32_t dim = 0 ; dim < input_data_ndim ; dim++){
+        output_shape[dim] = input_data_dims[dim];
+    }
 }
