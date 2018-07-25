51cbe7253cd39dadc3ea330894357aca0016ad2f
Akio Lin <akioolin@gmail.com>
2018-07-25 23:44:59 +0800

    add operator sigmoid

--- src/lib/operator/sigmoid.c
+++ src/lib/operator/sigmoid.c
@@ -4,6 +4,7 @@
 
 #include <stdint.h>
 #include <stdbool.h>
+#include <math.h>
 
 void ONNC_RUNTIME_sigmoid_float(
   void * restrict onnc_runtime_context
@@ -13,4 +14,16 @@ void ONNC_RUNTIME_sigmoid_float(
   ,int32_t output_Y_ndim, const int32_t * restrict output_Y_dims
   
 ) {
+	int32_t size = 1;
+	int32_t numofdim = input_X_ndim;
+
+	for(int32_t i = 0 ; i < numofdim ; ++i){
+		size *= input_X_dims[i];
+	}
+
+    //y = 1 / (1 + exp(-x))
+	for(int32_t i = 0 ; i < size ; ++i){
+		output_Y[i] = 1.0f / (1.0f + expf(input_X[i] * -1.0f));
+	}
 }
+
