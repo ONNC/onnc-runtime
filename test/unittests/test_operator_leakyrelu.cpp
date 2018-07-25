0d667b387be3468a05d3de5207e37f6f4ded2a41
LuisHsu <luishsuplus@gmail.com>
2018-07-25 18:37:33 +0800

    Leakyrelu unittest

---
+++ test/unittests/test_operator_leakyrelu.cpp
@@ -0,0 +1,46 @@
+#include <skypat/skypat.h>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+
+#define restrict __restrict__
+extern "C"{
+#include <operator/leakyrelu.h>
+}
+#undef restrict
+
+SKYPAT_F(Operator_Leakyrelu, non_broadcast){
+    // Prepare
+    srand(time(NULL));
+    int32_t ndim = rand() % 3 + 1;
+    int32_t dims[ndim];
+    int32_t dataSize = 1;
+    for(int32_t i = 0; i < ndim; ++i){
+        dims[i] = rand() % 100 + 1;
+        dataSize *= dims[i];
+    }
+    float A[dataSize], B[dataSize], Ans[dataSize];
+    float alpha = rand() % 1000 / 100;
+    for(int32_t i = 0; i < dataSize; ++i){
+        A[i] = rand() % 1000 / 100;
+        Ans[i] = (A[i] < 0) ? alpha * A[i] : A[i];
+    }
+    // Run
+    ONNC_RUNTIME_leakyrelu_float(NULL
+        ,A
+        ,ndim,dims
+        ,B
+        ,ndim,dims
+        ,alpha
+    );
+    // Check
+    for(int32_t i = 0; i < dataSize; ++i){
+        EXPECT_EQ(B[i], Ans[i]);
+    }
+}
+
+int main(int argc, char *argv[]){
+    skypat::Test::Initialize(argc, argv);
+    skypat::Test::RunAll();
+    return (skypat::testing::UnitTest::self()->getNumOfFails() == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
