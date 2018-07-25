8d73c53e63928fba94bfbfed1b4502360aa26997
LuisHsu <luishsuplus@gmail.com>
2018-07-26 03:20:06 +0800

    Affine unittest

---
+++ test/unittests/test_operator_affine.cpp
@@ -0,0 +1,48 @@
+#include <skypat/skypat.h>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+
+#define restrict __restrict__
+extern "C"{
+#include <operator/affine.h>
+}
+#undef restrict
+
+SKYPAT_F(Operator_Affine, non_broadcast){
+    // Prepare
+    srand(time(NULL));
+    int32_t ndim = rand() % 3 + 1;
+    int32_t dims[ndim];
+    int32_t dataSize = 1;
+    for(int32_t i = 0; i < ndim; ++i){
+        dims[i] = rand() % 100 + 1;
+        dataSize *= dims[i];
+    }
+    float A[dataSize], B[dataSize], Ans[dataSize];
+    float alpha = rand() % 10000 / 100;
+    float beta = rand() % 10000 / 100;
+    for(int32_t i = 0; i < dataSize; ++i){
+        A[i] = rand() % 10000 / 100;
+        Ans[i] = alpha * A[i] + beta;
+    }
+    // Run
+    ONNC_RUNTIME_affine_float(NULL
+        ,A
+        ,ndim,dims
+        ,B
+        ,ndim,dims
+        ,alpha
+        ,beta
+    );
+    // Check
+    for(int32_t i = 0; i < dataSize; ++i){
+        EXPECT_EQ(B[i], Ans[i]);
+    }
+}
+
+int main(int argc, char *argv[]){
+    skypat::Test::Initialize(argc, argv);
+    skypat::Test::RunAll();
+    return (skypat::testing::UnitTest::self()->getNumOfFails() == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
