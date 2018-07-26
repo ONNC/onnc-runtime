ac621198bb4e0cfc7403e23fad70ad55ada629d5
iknowright <changchaishi@gmail.com>
2018-07-26 12:24:28 +0800

    Operator Neg  Completed

--- src/lib/operator/neg.c
+++ src/lib/operator/neg.c
@@ -2,6 +2,7 @@
 
 #include <stdint.h>
 #include <stdbool.h>
+#include <stdio.h>
 
 void ONNC_RUNTIME_neg_float(
   void * restrict onnc_runtime_context
@@ -11,4 +12,15 @@ void ONNC_RUNTIME_neg_float(
   ,int32_t output_Y_ndim, const int32_t * restrict output_Y_dims
   
 ) {
+  
+  int32_t i;
+  int32_t dataSize = 1;
+  for (i = 0; i < input_X_ndim; i++){
+    dataSize *= input_X_dims[i];
+  }
+  
+  int32_t j;
+  for(j = 0; j < dataSize; j++){
+    output_Y[j] = -input_X[j];
+  }
 }
