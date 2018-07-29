e8258150d808e518875687a67fd074f50abc1aee
Akio Lin <akioolin@gmail.com>
2018-07-30 00:55:03 +0800

    add operator selu

--- src/lib/operator/selu.c
+++ src/lib/operator/selu.c
@@ -2,6 +2,7 @@
 
 #include <stdint.h>
 #include <stdbool.h>
+#include <math.h>
 
 void ONNC_RUNTIME_selu_float(
   void * restrict onnc_runtime_context
@@ -12,4 +13,21 @@ void ONNC_RUNTIME_selu_float(
   ,float alpha
   ,float gamma
 ) {
+	int32_t size = 1;
+
+	for(int32_t i = 0 ; i < input_X_ndim ; ++i){
+		size *= input_X_dims[i];
+	}
+
+    // y = gamma * (alpha * e^x - alpha) for x <= 0
+    // y = gamma * x for x > 0
+
+    // X = (X > 0) ? X : alpha *(expf(X) - 1.0f)
+    // y = gamma * X;
+	for(int32_t i = 0 ; i < size ; ++i){
+	    float tmp_val = input_X[i];
+        if(tmp_val <= 0.0f) tmp_val = alpha * (expf(tmp_val) - 1.0f);
+        output_Y[i] = gamma * tmp_val;
+	}
 }
+
