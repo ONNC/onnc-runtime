f4f9ce536d30bef38be53ee64368409c73d1f1c9
iknowright <changchaishi@gmail.com>
2018-07-29 18:25:49 +0800

    update randomnormal

--- src/lib/operator/randomnormal.c
+++ src/lib/operator/randomnormal.c
@@ -2,10 +2,13 @@
 
 #include <stdint.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <math.h>
+
+static float randomNormal(float a, float b, float seed);
 
 void ONNC_RUNTIME_randomnormal_float(
-  void * restrict onnc_runtime_context
-  
+  void * restrict onnc_runtime_context  
   ,float * restrict output_output
   ,int32_t output_output_ndim, const int32_t * restrict output_output_dims
   ,int32_t dtype
@@ -15,4 +18,27 @@ void ONNC_RUNTIME_randomnormal_float(
   ,int32_t * restrict shape
   ,int32_t number_of_shape
 ) {
+  number_of_shape = output_output_ndim;
+
+  int32_t dataSize = 1;
+  for(int32_t i = 0; i < output_output_ndim; i++){
+    shape[i] = output_output_dims[i];
+    dataSize *= output_output_dims[i];
+  }
+
+  for(int32_t i = 0; i < dataSize; i++){
+    output_output[i] = randomNormal(mean, scale, seed);
+  }
+}
+
+static float randomNormal(float mean, float stddev, float seed)
+{
+  srand(seed);
+  double pi = acos(-1);
+  float x = (float)random() / RAND_MAX + 1;
+  float y = (float)random() / RAND_MAX + 1;
+  float z = sqrt(-2 * log(x)) * cos(2 * pi * y);
+  z = mean + stddev *  z;
+  
+  return z;
 }
