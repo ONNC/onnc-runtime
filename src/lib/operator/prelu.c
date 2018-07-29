6a7a0473aa089007c1a2dbc1171aaf4fcfd689ad
Akio Lin <akioolin@gmail.com>
2018-07-30 00:48:26 +0800

    add operator prelu

--- src/lib/operator/prelu.c
+++ src/lib/operator/prelu.c
@@ -13,4 +13,17 @@ void ONNC_RUNTIME_prelu_float(
   ,int32_t output_Y_ndim, const int32_t * restrict output_Y_dims
   
 ) {
+	int32_t size = 1;
+
+	for(int32_t i = 0 ; i < input_X_ndim ; ++i){
+		size *= input_X_dims[i];
+	}
+
+    // f(x) = slope * x for x < 0, f(x) = x for x >= 0
+	for(int32_t i = 0 ; i < size ; ++i){
+	    float tmp_val = input_X[i];
+		float slope_val = input_slope[i];
+		output_Y[i] = (tmp_val >= 0.0f) ? tmp_val : tmp_val * slope_val;
+	}
 }
+
