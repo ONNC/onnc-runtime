c7ce8a8a6619ec95514c82e655242749c72c3d42
LuisHsu <luishsuplus@gmail.com>
2018-07-30 02:34:43 +0800

    Using find instead of externL project

--- cmake/Findonnx.cmake
+++ cmake/Findonnx.cmake
@@ -1,15 +1,10 @@
-option(ONNX_PATH "Path of onnx library")
 find_library(ONNX_LIBRARIES
     NAMES onnx
-    HINTS ${ONNX_PATH}
-    HINTS ${ONNC_RUNTIME_ROOT}/onnx/build/
 )
-link_directories(${ONNX_LIBRARIES})
 find_path(ONNX_INCLUDE_DIR
-    NAMES onnx_pb.h
-    HINTS ${ONNX_PATH}
-    HINTS ${ONNC_RUNTIME_ROOT}/onnx/onnx/
+    NAMES onnx/onnx.pb.h
 )
-include_directories(${ONNX_INCLUDE_DIR})
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(ONNX DEFAULT_MSG ONNX_INCLUDE_DIR ONNX_LIBRARIES)
+link_directories(${ONNX_LIBRARIES})
+include_directories(${ONNX_INCLUDE_DIR})
