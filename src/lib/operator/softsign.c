a80bae6dd778cff3e62cd2d964551cf1d7f8ac9b
Akio Lin <akioolin@gmail.com>
2018-07-30 22:04:38 +0800

    add operator softsign

--- src/lib/operator/softsign.c
+++ src/lib/operator/softsign.c
@@ -2,6 +2,7 @@
 
 #include <stdint.h>
 #include <stdbool.h>
+#include <math.h>
 
 void ONNC_RUNTIME_softsign_float(
   void * restrict onnc_runtime_context
@@ -11,4 +12,16 @@ void ONNC_RUNTIME_softsign_float(
   ,int32_t output_output_ndim, const int32_t * restrict output_output_dims
   
 ) {
+	int32_t size = 1;
+
+	for(int32_t i = 0 ; i < input_input_ndim ; ++i){
+		size *= input_input_dims[i];
+	}
+
+    //y = (x/(1+|x|)) 
+	for(int32_t i = 0 ; i < size ; ++i){
+	    float tmp_val = input_input[i];
+        output_output[i] = tmp_val / (fabsf(tmp_val) + 1.0f);
+	}
 }
+
