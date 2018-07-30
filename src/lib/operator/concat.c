83a67577ac3f6ee7b27a3eb3ea63fef0253a9190
TDYa127 <a127a127@skymizer.com>
2018-07-30 16:22:09 +0800

    Re generate code.

--- src/lib/operator/concat.c
+++ src/lib/operator/concat.c
@@ -5,9 +5,9 @@
 
 void ONNC_RUNTIME_concat_float(
   void * restrict onnc_runtime_context
-  ,const float ** restrict input_inputs
+  ,const float * const * restrict input_inputs
   ,int32_t input_inputs_ntensor
-  ,int32_t * input_inputs_ndim, const int32_t ** restrict input_inputs_dims
+  ,const int32_t * input_inputs_ndim, const int32_t * const * restrict input_inputs_dims
   ,float * restrict output_concat_result
   ,int32_t output_concat_result_ndim, const int32_t * restrict output_concat_result_dims
   ,int32_t axis
--- src/lib/operator/min.c
+++ src/lib/operator/min.c
@@ -5,9 +5,9 @@
 
 void ONNC_RUNTIME_min_float(
   void * restrict onnc_runtime_context
-  ,const float ** restrict input_data_0
+  ,const float * const * restrict input_data_0
   ,int32_t input_data_0_ntensor
-  ,int32_t * input_data_0_ndim, const int32_t ** restrict input_data_0_dims
+  ,const int32_t * input_data_0_ndim, const int32_t * const * restrict input_data_0_dims
   ,float * restrict output_min
   ,int32_t output_min_ndim, const int32_t * restrict output_min_dims
   
